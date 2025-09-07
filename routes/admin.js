b6ccad2f0a32d82c7ef87ca60644dd1660cd6314
Mahmoud AlEbrahim <ma453935@gmail.com>
2025-09-07 07:26:46 +0300

    .

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const adminController = require('../controllers/adminController');
 
 
-router.get("/all-rest", adminController.getAllRestaurant);
+router.get("/restaurants", adminController.getAllRestaurant);
 router.put('/:id/verify',adminController.changeRestaurantStatus);
 router.get("/all-users", adminController.getAllUsers);
 router.get("/all-orders", adminController.getAllOrders);
