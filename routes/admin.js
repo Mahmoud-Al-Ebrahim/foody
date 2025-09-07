ced03da19928098150bcca359742ab19b776387c
Mahmoud AlEbrahim <ma453935@gmail.com>
2025-09-07 07:32:34 +0300

    .

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const adminController = require('../controllers/adminController');
 
 router.get("/restaurants", adminController.getAllRestaurant);
 router.put('/:id/verify',adminController.changeRestaurantStatus);
-router.get("/all-users", adminController.getAllUsers);
+router.get("/users", adminController.getAllUsers);
 router.get("/all-orders", adminController.getAllOrders);
 router.put('/:id/verify-driver',adminController.chnageDriverStatus);
 module.exports = router;
