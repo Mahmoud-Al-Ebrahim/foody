4c4a55406d9d9919d20dcda7d88affaf202a6422
Mahmoud AlEbrahim <ma453935@gmail.com>
2025-09-08 12:23:56 +0300

    .

--- routes/user.js
+++ routes/user.js
@@ -16,8 +16,8 @@ router.get("/verify/:otp", verifyTokenAndAuthorization, userController.verifyAcc
 
 router.get("/verify_phone/:phone", verifyTokenAndAuthorization, userController.verifyPhone);
 
-router.post('/:userId/favorites/:restId' , verifyTokenAndAuthorization, userController.addFavorites);
-router.delete('/:userId/favorites/:restId',verifyTokenAndAuthorization, userController.removeFavorites);
+router.post('/:userId/favorites/:restaurantId' , verifyTokenAndAuthorization, userController.addFavorites);
+router.delete('/:userId/favorites/:restaurantId',verifyTokenAndAuthorization, userController.removeFavorites);
 router.get('/:userId/favorites',verifyTokenAndAuthorization, userController.getFavorites);
 
 module.exports = router;
